7e4e09ecf9ab5458a317548fbbcca1199e1fb79b
四柳 貴光 <takamitsu.yotsu@DM130056.local>
2018-12-11 17:45:02 +0900

    first commit

---
+++ RxPracticeTests/RxPracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxPracticeTests.swift
+//  RxPracticeTests
+//
+//  Created by 四柳 貴光 on 2018/12/11.
+//  Copyright © 2018年 giiiita. All rights reserved.
+//
+
+import XCTest
+@testable import RxPractice
+
+class RxPracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
